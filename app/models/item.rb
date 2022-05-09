8cdb28fa5dd1aa6527d842afb0beba1db1522d65
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-05-09 11:25:27 -0400

    more authentifications

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to :category
+  
   has_many :images
   has_many :comments
+  has_many :categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete "/items/:id" => "items#destroy"
 
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
   
   root "items#index"
 
