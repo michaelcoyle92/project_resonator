07f6d786e8f89d4399b39d79d1fe9c77dd7a1f47
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-05-09 06:43:21 -0400

    templates refined

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,35 +1,36 @@
 class ItemsController < ApplicationController
   def index
-    items = Item.all
-    render json: items.as_json
+    @items = Item.all
+    # render json: items.as_json
+    render template: "items/index"
   end
 
   def create
-    item = Item.new(
+    @item = Item.new(
       name: params[:name],
       manufacturer: params[:manufacturer],
       year: params[:year],
       description: params[:description],
       item_id: params[:item_id]
     )
-    item.save
-    render json: item.as_json
+    @item.save
+    render template: "items/show"
   end
   
   def show
-    item = Item.find_by(id: params[:id])
-    render json: item.as_json
+    @item = Item.find_by(id: params[:id])
+    render template: "items/show"
   end
 
   def update
-    item = Item.find_by(id: params[:id])
-    item.name = params[:name] || item.name
-    item.manufacturer = params[:manufacturer] || item.manufacturer
-    item.year = params[:year] || item.year
-    item.description = params[:description] || item.description
-    item.item_id = params[:item_id] || item.item_id
-    item.save
-    render json: item.as_json
+    @item = Item.find_by(id: params[:id])
+    @item.name = params[:name] || @item.name
+    @item.manufacturer = params[:manufacturer] || @item.manufacturer
+    @item.year = params[:year] || @item.year
+    @item.description = params[:description] || @item.description
+    @item.item_id = params[:item_id] || @item.item_id
+    @item.save
+    render template: "items/show"
   end
 
   def destroy
